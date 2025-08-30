e9b95b242b7ed31fc8549e540c4f78ffdc114e7e
jorgegrande2110 <jorgegrandenistal@gmail.com>
2025-08-31 01:30:21 +0200

    Create ejercicios.sql

---
+++ ejercicios.sql
@@ -0,0 +1,9 @@
+Select fligth_id, flight_no, status FROM flights WHERE status = 'On time';
+
+Select * from bookings WHERE total_amount >= 100000;
+
+Select * from aircrafts_data;
+
+Select flight_id, flight_no from flights WHERE aircraft_code = '733'
+
+Select * from tickets WHERE passenger_name LIKE 'IRINA%'
